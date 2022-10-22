fbb1e7f73a2aa00707824a94ce4d1fa61e59cd41
Arber Bajraktari <arberbajraktari@gmail.com>
2022-10-22 17:55:39 +0200

    Regex added for BL Validation

--- src/main/java/at/fhtw/swen3/persistence/entityForThree/ParcelEntity.java
+++ src/main/java/at/fhtw/swen3/persistence/entityForThree/ParcelEntity.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Getter
@@ -15,8 +17,11 @@ public class ParcelEntity {
     //This entity unites 3 DTOs (Parcel, TrackingInformation and NewParcelInfo)
 
     //Parcel
+    @Min(value = 0, message = "Weight should be bigger than 0,0")
     private Float weight;
+    @NotNull
     private Recipient recipient;
+    @NotNull
     private Recipient sender;
 
     //TrackingInformation
