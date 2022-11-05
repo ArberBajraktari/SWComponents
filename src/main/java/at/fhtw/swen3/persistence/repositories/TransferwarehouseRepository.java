90e8c7c4eb8f771ca03ec0a4aa60a7fedd9bddd9
ArberBajraktari <arberbajraktari02@gmail.com>
2022-11-05 22:35:29 +0100

    Repositories added :)

---
+++ src/main/java/at/fhtw/swen3/persistence/repositories/TransferwarehouseRepository.java
@@ -0,0 +1,8 @@
+package at.fhtw.swen3.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferwarehouseRepository extends JpaRepository {
+}
