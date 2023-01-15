f88fd0d5ee4551160876acefc11ac246930cfbab
Anis Shkembi <anis.shkembi@gmail.com>
2023-01-15 22:55:45 +0100

    All tests working

--- src/test/java/at/fhtw/swen3/services/impl/ParcelServiceImplTest.java
+++ src/test/java/at/fhtw/swen3/services/impl/ParcelServiceImplTest.java
@@ -1,8 +1,7 @@
 package at.fhtw.swen3.services.impl;
 
-import at.fhtw.swen3.persistence.entities.HopArrivalEntity;
-import at.fhtw.swen3.persistence.entities.ParcelEntity;
-import at.fhtw.swen3.persistence.entities.RecipientEntity;
+import at.fhtw.swen3.gps.service.impl.BingEncodingProxy;
+import at.fhtw.swen3.persistence.entities.*;
 import at.fhtw.swen3.services.BLException;
 import at.fhtw.swen3.services.dto.Parcel;
 import at.fhtw.swen3.services.mapper.ParcelMapperImpl;
@@ -12,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
+import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,14 +25,13 @@ class ParcelServiceImplTest {
     public ParcelServiceImpl parcelServiceImpl;
 
 
-    final RecipientEntity recipientEntity = new RecipientEntity(2, "Mreti", "Dobrac 12/12",
-            "1010", "Vienna", "Austria");
-    final RecipientEntity senderEntity = new RecipientEntity(1, "Mreti", "Dobrac 12/12",
-            "1010", "Vienna", "Austria");
+    final RecipientEntity recipientEntity = new RecipientEntity(2, "Mreti", "Leystrasse 131/12",
+            "1200", "Vienna", "Austria");
+    final RecipientEntity senderEntity = new RecipientEntity(1, "Mretja", "Leystrasse 129/12",
+            "1200", "Vienna", "Austria");
     final List<HopArrivalEntity> visitedHops = new ArrayList<>();
     final ParcelEntity parcelEntity = new ParcelEntity(1, 10.0f, recipientEntity,
             senderEntity, "Pickup", visitedHops, visitedHops, "ABCK12345");
-
 
     @Test
     void submitNewParcel() {
@@ -44,16 +43,4 @@ class ParcelServiceImplTest {
             e.printStackTrace();
         }
     }
-
-//    @Test
-//    void getParcelByTrackingId() {
-//        try {
-//            ParcelEntity parcelEntityRes = parcelServiceImpl.getParcelByTrackingId("ABCK12345");
-//            System.out.println(parcelEntityRes.getTrackingId());
-////            Parcel parcel = ParcelMapperImpl.INSTANCE.entityToDto(parcelEntityRes);
-////            System.out.println(parcel.toString());
-//        } catch (BLException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
